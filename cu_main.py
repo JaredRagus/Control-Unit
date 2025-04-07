138f87f212d57a067041e49a5e142e990719c29c
dkunz60 <kunzd60@gmail.com>
2025-04-06 20:10:15 -0400

    Update cu_main.py

--- cu_main.py
+++ cu_main.py
@@ -6,17 +6,15 @@ import RPi.GPIO as GPIO
 from paho import mqtt
 import json
 import subprocess
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 
+sensor_data_file = '/home/freshair/sensor_data.txt'
 # Initialize variables
 toggle = False
 connection_status = False
 new_data = False
 user_input = False
-sensor = 0
-Sensor_1 = False
-Sensor_2 = False
-Sensor_3 = False
-Sensor_4 = False
 sensor_id = ""
 received = False
 vent = False
@@ -25,13 +23,26 @@ IAQ = 0 #UI Variable
 IAQ_PM2 = 0
 IAQ_PM10 = 0
 IAQ_ovr = 0
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-GPIO.setup(16,GPIO.OUT, initial = False)                  
-GPIO.setup(18,GPIO.OUT, initial = False)
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setwarnings(False)
+# GPIO.setup(16,GPIO.OUT, initial = False)                  
+# GPIO.setup(18,GPIO.OUT, initial = False)
+GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # Need a new variable for sensor id
-UI_command = ["alarms","/home/freshair/Documents/PM_Device/Alarms.py",IAQ,sensor]
+UI_command = ["alarms","/home/freshair/Documents/PM_Device/Alarms.py",IAQ,sensor_id]
+def reset_sensors():
+    # Erase the sensor_id file if it exists
+    if os.path.exists(sensor_data_file):
+        os.remove(sensor_data_file)
+        print(f"{sensor_data_file} has been erased.")
 
+    # Initialize sensor values to 1
+    global sensor_1, sensor_2, sensor_3, sensor_4
+    sensor_1 = 1
+    sensor_2 = 1
+    sensor_3 = 1
+    sensor_4 = 1
+    print("All sensor values initialized to 1.")
 # Raw data from sensors comes in the following format:
 # {"iaqi_pm2.5":12,"iaqi_pm10":2,"overall_iaqi":12}
 # NOTE: Numeric values between ":" and "," characters will change
@@ -63,23 +74,15 @@ def on_message(client, userdata, msg):
     global received
     global toggle
     global vent
-    global Sensor_1
-    global Sensor_2
-    global Sensor_3
-    global Sensor_4
     global sensor_id
-    
-    # If message from Toggle, button was pressed, set to true
-    if "Toggle" in msg.topic:
-        toggle = True
-        
+            
     # If message from Ventilation, set equal to message value
-    if "Vent" in msg.topic:
+    if "Status" in msg.topic:
         vent_payload = msg.payload.decode("utf-8").strip().lower()
     
-        if vent_payload in ['true', '1', b'true', b'1']:
+        if vent_payload in ['true', '1', b'true', b'1','on','ON']:
             vent = True
-        elif vent_payload in ['false', '0', b'false', b'0']:
+        elif vent_payload in ['false', '0', b'false', b'0','off','OFF']:
             vent = False
         else:
             vent = False
@@ -87,32 +90,18 @@ def on_message(client, userdata, msg):
         received = True
         
     # If message is from sensor, parse data 
-    if "Sensor" in msg.topic:
+    if "air_quality" in msg.topic:
         payload = msg.payload.decode("utf-8")
         data = json.loads(payload)
         print("Received message: ", data)
         received = True
-        
-    # Determine sensor ID
-    if "Sensor1" in msg.topic:
-        Sensor_1 = True
-        sensor_id = "1"
-    if "Sensor2" in msg.topic:
-        Sensor_2 = True
-        sensor_id = "2"
-    if "Sensor3" in msg.topic:
-        Sensor_3 = True
-        sensor_id = "3"
-    if "Sensor4" in msg.topic:
-        Sensor_4 = True
-        sensor_id = "4"
      
 # MQTT Initialization
 client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
 client.on_connect = on_connect
-client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+#client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS) # No need for TLS in the full product, only for testing in HIVEMQ
 client.username_pw_set("freshair", "freshair")
-client.connect("mqtt://155.225.212.205:1883", 1883)
+client.connect("localhost", 1883)
 client.on_subscribe = on_subscribe
 client.on_message = on_message
 client.on_publish = on_publish
@@ -129,23 +118,30 @@ while connection_status == True:
     time.sleep(0.5)
     
     # If acknowledged, turn off sirens
-    while toggle == True:
-        GPIO.output(18, False)
-        time.sleep(0.5)
-        GPIO.output(16, False)
-        time.sleep(0.5)
-        print("Siren Status: OFF")
-        toggle = False
+#     while toggle == True:
+#         GPIO.output(18, False)
+#         time.sleep(0.5)
+#         GPIO.output(16, False)
+#         time.sleep(0.5)
+#         print("Siren Status: OFF")
+#         toggle = False
             
     # If data received
     while received == True:
-        if Sensor_1 == True or Sensor_2 == True or Sensor_3 == True:
-            # Parse sensor data
-            IAQ_PM2 = data.get("iaqi_pm2.5", 0)
-            IAQ_PM10 = data.get("iaqi_10", 0)
-            IAQ_ovr = data.get("overall_iaqi", 0)
-            new_data = True
-            received = False
+        # Parse sensor data
+        sensor_id = data.get("SensorID", "Unknown")  # Default to 'Unknown' if SensorID is missing
+        #pm2_5 = data.get("PM2.5 ug/m^3", 0.0)  # Default to 0.0 if PM2.5 value is missing
+        #pm10 = data.get("PM10 ug/m^3", 0.0)  # Default to 0.0 if PM10 value is missing
+        IAQ_PM2 = data.get("IAQI_PM2.5", 0)  # Default to 0 if IAQI_PM2.5 is missing
+        IAQ_PM10 = data.get("IAQI_PM10", 0)  # Default to 0 if IAQI_PM10 is missing
+        IAQ_ovr = data.get("Overall_IAQI", 0)  # Default to 0 if Overall_IAQI is missing
+        print(f"Sensor ID: {sensor_id}")
+#         print(f"PM2.5 (ug/m^3): {pm2_5}")
+#         print(f"PM10 (ug/m^3): {pm10}")
+        print(f"IAQI PM2.5: {IAQ_PM2}")
+        print(f"IAQI PM10: {IAQ_PM10}")
+        print(f"Overall IAQI: {IAQ_ovr}")
+        new_data = True
         
         # Handle ventilation status
         if vent == True:
@@ -162,55 +158,52 @@ while connection_status == True:
         time.sleep(0.5)
                 
         # If over/equal to conditions below, "Dangerous" PM condition met (via OSHA 1910.1000)
-        if IAQ_PM2 >= 55 or IAQ_PM10 >= 255 or IAQ_ovr >=255:
-            client.publish("ESP32/Control", payload="ON", qos=0)
+        if IAQ_ovr >=200:
+            client.publish("ESP32/Relay", payload="ON", qos=0)
             time.sleep(0.5)
             IAQ = "3"
-            IAQ += sensor_id
-            client.publish("cu/vent", payload=True, qos=0)
-            print("Ventilation Status: ON")
+            time.sleep(0.3)
+            #IAQ += sensor_id
+            #client.publish("cu/vent", payload=True, qos=0)
+            print("Dangerous")
             time.sleep(0.5)
-            subprocess.run(['python','/home/freshair/Documents/Testing_Codes/User_Interface/Testing_Plan_Main.py', IAQ] ,text=True, timeout=1)
-            time.sleep(5)
+            subprocess.run(['python','/home/freshair/Documents/Testing_Codes/User_Interface/Testing_Plan_Main_Copy.py', IAQ, sensor_id] ,text=True, timeout=3)
+            time.sleep(0.5)
             new_data = False
             
         # If under "Dangerous" threshold and over/equal to conditions below, "Unhealthy" PM condition met (via OSHA 1910.1000)
-        elif IAQ_PM2 >= 35 or IAQ_PM10 >= 155 or IAQ_ovr >=155:
-            client.publish("cu/pm_status", payload="Unhealthy", qos=0)
-            print("Siren Status: YELLOW")
+        elif IAQ_ovr >=100:
+            #client.publish("cu/pm_status", payload="Unhealthy", qos=0)
+            print("Unhealthy")
+            time.sleep(0.3)
             IAQ = "2"
-            IAQ += sensor_id
+            #IAQ += sensor_id
             time.sleep(0.5)
-            subprocess.run(['python','/home/freshair/Documents/Testing_Codes/User_Interface/Testing_Plan_Main.py', IAQ] ,text=True, timeout=1)
-            time.sleep(5)
+            subprocess.run(['python','/home/freshair/Documents/Testing_Codes/User_Interface/Testing_Plan_Main_Copy.py', IAQ, sensor_id] ,text=True, timeout=3)
+            time.sleep(0.5)
             new_data = False
             
         # If below both conditions, "Healthy" PM condition met (via OSHA 1910.1000)
         else:
-            client.publish("ESP32/Control", payload="OFF", qos=0)
+            time.sleep(0.3)
+            client.publish("ESP32/Relay", "OFF", qos=0)
+            time.sleep(0.3)
             IAQ = "1"
-            IAQ += sensor_id
+            time.sleep(0.3)
+            print("Healthy")
+            #IAQ += sensor_id
             time.sleep(0.5)
-            subprocess.run(['python','/home/freshair/Documents/Testing_Codes/User_Interface/Testing_Plan_Main.py', IAQ] ,text=True, timeout=1)
-            time.sleep(5)
-            print("Ventilation Status: OFF")
-            print("Siren Status: OFF")
+            subprocess.run(['python','/home/freshair/Documents/Testing_Codes/User_Interface/Testing_Plan_Main.py', IAQ, sensor_id] ,text=True, timeout=3)
+            time.sleep(0.5)
             toggle = True
             new_data = False
             
-            
-        # Publish sensor ID of the data to the broker    
-        if Sensor_1 == True:
-            client.publish("cu/sensor_id", payload="Sensor 1", qos=0)
-            print("Data received from Sensor 1")
-            # Save as previous status of Sensor 1, 2, 3, etc.
-        if Sensor_2 == True:
-            client.publish("cu/sensor_id", payload="Sensor 2", qos=0)
-            print("Data received from Sensor 2")
-        if Sensor_3 == True:
-            client.publish("cu/sensor_id", payload="Sensor 3", qos=0)
-            print("Data received from Sensor 3")           
-            
+def button_callback(channel):
+    GPIO.output(18,False)
+    GPIO.output(16, False)
+    
+GPIO.add_event_detect(37,GPIO.RISING,callback=button_callback)
+reset_sensors()
 GPIO.cleanup()
 client.loop_stop()
 sys.exit()
